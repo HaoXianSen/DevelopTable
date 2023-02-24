9535b80f53719259eaad8edae354085aa23a79fc
郝玉鸿 <haoyh02@rd.netease.com>
2023-02-24 19:40:42 +0800

    添加选择podfile地址逻辑

--- DevelopTable/ViewController.swift
+++ DevelopTable/ViewController.swift
@@ -9,6 +9,11 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var podfilePath: NSTextField!
+    
+    @IBOutlet weak var initButton: NSButton!
+    
+    @IBOutlet weak var selecteFileButton: NSButton!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,6 +26,40 @@ class ViewController: NSViewController {
         }
     }
 
+    @IBAction func initAction(_ sender: Any) {
+    }
+    
+    @IBAction func selectFileAction(_ sender: NSButton) {
+        openFileSelectePanel()
+    }
+}
 
+extension ViewController: NSOpenSavePanelDelegate {
+    func openFileSelectePanel()  {
+        let panel = NSOpenPanel()
+        panel.message = "请选择Podfile文件"
+        panel.allowedFileTypes = [""]
+        panel.allowsMultipleSelection = false
+        let path = URL(string: "\(NSHomeDirectory())/Desktop")
+        panel.directoryURL = path
+        panel.delegate = self
+        panel.begin { modelResponse in
+            if modelResponse == NSApplication.ModalResponse.OK {
+                guard let path = panel.url, path.lastPathComponent == "Podfile" else {
+                    let alert = NSAlert()
+                    alert.messageText = "请选择Podfile文件！"
+                    alert.icon = NSImage(named: "error_icon")
+                    alert.alertStyle = .warning
+                    alert.runModal()
+                    return
+                }
+                self.podfilePath.placeholderString = path.absoluteString
+            }
+        }
+    }
+    
+//    func panel(_ sender: Any, shouldEnable url: URL) -> Bool {
+//        return url. && url.lastPathComponent == "Podfile"
+//    }
 }
 
---
+++ Podfile
@@ -0,0 +1,19 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'DevelopTable' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for DevelopTable
+
+  target 'DevelopTableTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'DevelopTableUITests' do
+    # Pods for testing
+  end
+
+end
