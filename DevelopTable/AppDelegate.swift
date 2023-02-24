1621fe14b3712b96db40dab244222ca6d5827ad7
郝玉鸿 <haoyh02@rd.netease.com>
2023-02-24 17:54:01 +0800

    init

---
+++ DevelopTable/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  DevelopTable
+//
+//  Created by 郝玉鸿 on 2023/2/24.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
